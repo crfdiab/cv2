c2ac06c6778d8b4181ce96cfcb699867652416c0
crfdiab <trickyaffiliate.com@gmail.com>
2024-10-06 04:57:53 +0300

    Fixed

--- settings/settings.ts
+++ settings/settings.ts
@@ -4,7 +4,7 @@ export const siteicon = "/icon.png";
 export const gtm = "GTM-XXXXXXX";
 export const gtmconnected = true;
 
-export const sitename = "AI Curator";
+export const sitename = "AI Curator Directory";
 export const description = "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.";
 export const keywords = ["Next.js", "documentation", "Rubix Studios", "open source", "SEO"];
 export const urlimage = "/images/og-image.png";
